4c78259125cc3c4d8400c0f787e09447baac6f2b
Jose Gonzalez <gon81986@ttu.edu>
2026-04-10 23:40:32 -0500

    lab05&lab06 complete

--- lab06/Question1/student.c
+++ lab06/Question1/student.c
@@ -11,8 +11,17 @@
 
 
 int count_edges(Graph* g) {
-    // TODO: implement
-    // return -1;
+      int count = 0;
+
+    for (int i = 0; i < MAX_NODES; i++) {
+        for (int j = 0; j < MAX_NODES; j++) {
+            if (g->adj[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+
+    return count / 2;
 
 }
 
--- lab06/Question2/student.c
+++ lab06/Question2/student.c
@@ -11,8 +11,24 @@
 
 
 int count_isolated(Graph* g) {
-    // TODO: implement
-    // return -1;
+   int isolatedCount = 0;
+
+    for (int i = 0; i < MAX_NODES; i++) {
+        int isIsolated = 1;  // assume isolated
+
+        for (int j = 0; j < MAX_NODES; j++) {
+            if (g->adj[i][j] == 1) {
+                isIsolated = 0;  // found an edge
+                break;
+            }
+        }
+
+        if (isIsolated) {
+            isolatedCount++;
+        }
+    }
+
+    return isolatedCount;
    
 }
 
