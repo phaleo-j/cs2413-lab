bf2b975d5d68955b931f1c578dc6383393d4deee
Jose Leonardo Gonzalez <phaleojose_@hotmail.com>
2026-05-12 00:58:48 -0500

    Update student.c

--- lab07/Question3/student.c
+++ lab07/Question3/student.c
@@ -1,3 +1,6 @@
+#include <stddef.h>
+
+
 /*
 Question 3: Quick Sort
 
@@ -27,14 +30,51 @@ Example:
 Input:  [10, 7, 8, 9, 1, 5]
 Output: [1, 5, 7, 8, 9, 10]
 
+pivot: 5
+then, we simply do 
+[]
+[1,5,10,7,8,9]
+
+for
+
+
 Notes:
 - If the array is empty or has only one element, do nothing.
 - The sorting should be done in ascending order.
 */
 
+
 void quickSort(int arr[], int size) {
+
+    int temp = 0;
     // TODO: implement quick sort
-    (void)arr;
-    (void)size;
+    if (size == 1){                  //array with only 1 element
+        return;
+    }   
+    if ((arr == NULL) || (size <= 0)){  //empty array
+        return;
+
+    }
+
+    int pivot = arr[size - 1];  //choosing the last element as pivot
+    int placement_index = -1;
+
+    for(int i = 0; i < size - 1; i++){
+
+        if(arr[i] < pivot){ 
+            placement_index++;  //increment placement index
+            temp = arr[placement_index];
+            arr[placement_index] = arr[i];
+            arr[i] = temp;     //swap
+        }
+    }
+    
+temp = arr[placement_index + 1];    //place pivot at the correct index
+arr[placement_index + 1] = arr[size - 1];
+arr[size - 1] = temp;
+
+quickSort(arr, placement_index + 1);  //right partition and size
+quickSort(arr + placement_index + 2, size - placement_index - 2 );  //
+
 }
 
